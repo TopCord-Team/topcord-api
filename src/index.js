059768acccc14c161248f4e05d5117ce41482b4d
vitaliyirtlach <w13vitaliy@gmail.com>
2021-07-14 21:20:19 +0300

    feat: TopCord

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-const hello = () => "Hello World"
+const fetch = require("node-fetch")
+class TopCord {
+    #token = ""
+    #user  = {}
+    
+    constructor(token) {
+        this.#token = token
+        this.#getUserByToken(token)
+    }
 
-module.exports = { hello }
+    /**
+     * @description Return bot or undefined using TopCord API
+     * @param {string | number} id ID of bot (which was added in TopCord) from Discord Developer Portal.  
+     * @returns {Bot | undefined} 
+    */
+    async findBot(id) {
+
+    }
+
+    /**
+      * @description Return user or undefined using TopCord API
+      * @param {string | number} id ID of user (which was logged in TopCord).  
+      * @returns {User | undefined} 
+    */
+    async findUser(id) {
+    
+    }
+
+    
+
+    /**
+      * @description Get user info
+      * @param {string | number} token Token from TopCord Auth  
+    */
+    async #getUserByToken(token) {
+
+    }
+}
+
+module.exports = TopCord
